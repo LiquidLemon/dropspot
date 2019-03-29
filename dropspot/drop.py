904aef112c662ad43798ce37119cbcf3da94585c
FFugi <fudifil@gmail.com>
2019-03-29 20:16:53 +0100

    Add drop form

--- dropspot/drop.py
+++ dropspot/drop.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('drop', __name__)
+bp = Blueprint("drop", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 def index():
-    return render_template('drop.html')
+    return render_template("drop.html")
