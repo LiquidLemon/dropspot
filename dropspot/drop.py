c5d7b3ae02ff019afdcff7e5fdf348dfb4be57f1
FFugi <fudifil@gmail.com>
2019-03-29 23:32:19 +0100

    Add flash categories

--- dropspot/drop.py
+++ dropspot/drop.py
@@ -10,26 +10,27 @@ def index():
     if request.method == "POST":
         totp = TOTP(app.config["TOTP_SECRET"])
         if "authcode" not in request.form:
-            flash("Missing OTP")
+            flash("Missing OTP", 'error')
             return redirect(request.url)
 
         if not totp.verify(request.form["authcode"]):
-            flash("Incorrect OTP")
+            flash("Incorrect OTP", "error")
             return redirect(request.url)
 
         if not request.files:
-            flash("No files field")
+            flash("No files field", "error")
             return redirect(request.url)
 
         files = (f for f in request.files.getlist("files") if f.filename != "")
 
         if not files:
-            flash("No files specified")
+            flash("No files specified", "error")
             return redirect(request.url)
 
         for file in files:
             file.save(os.path.join(app.config["UPLOAD_DIR"], file.filename))
 
+        flash("Files uploaded correctly!", "success")
         return redirect("/drop")
 
     return render_template("drop.html")
