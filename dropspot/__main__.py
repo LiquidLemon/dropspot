4d92841500cd1c3f9253e58bae5248d20d0baf5c
LiquidLemon <liquidlemon@protonmail.com>
2019-03-29 20:17:07 +0100

    Add basic file upload

--- dropspot/__main__.py
+++ dropspot/__main__.py
@@ -4,7 +4,10 @@ from dropspot import auth, drop
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("../config.py", silent=True)
+    app.register_blueprint(auth.bp)
     app.register_blueprint(drop.bp)
+    app.add_url_rule("/", endpoint="drop")
     return app
 
 
--- dropspot/drop.py
+++ dropspot/drop.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect
+from flask import current_app as app
+import os
 
 bp = Blueprint("drop", __name__)
 
-
-@bp.route("/")
+@bp.route("/drop", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if not request.files:
+            flash("No files field")
+            redirect(request.url)
+
+        files = [f for f in request.files if f.filename != ""]
+        if not files:
+            flash("No files specified")
+            redirect(request.url)
+
+        for file in files:
+            file.save(os.path.join(app.config["UPLOAD_DIR"], file.filename))
+
+        return redirect("/")
+
     return render_template("drop.html")
