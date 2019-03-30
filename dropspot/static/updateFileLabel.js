4c920c54795eb4b9f108ec5dc32d2170645818d4
FFugi <fudifil@gmail.com>
2019-03-30 09:40:49 +0100

    Improve file input design

---
+++ dropspot/static/updateFileLabel.js
@@ -0,0 +1,4 @@
+function updateFileLabel(files) {
+  let form = files.length == 1 ? "file" : "files"
+  document.getElementById("files-label").innerHTML = files.length + " " + form + " selected";
+}
