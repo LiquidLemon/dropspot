5d7d4705e8f8d8c5064c72d38d43c845d154b020
FFugi <fudifil@gmail.com>
2019-04-02 10:36:29 +0200

    Add login template

--- dropspot/auth.py
+++ dropspot/auth.py
@@ -6,3 +6,8 @@ bp = Blueprint("auth", __name__)
 @bp.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
+
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
