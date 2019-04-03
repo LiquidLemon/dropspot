3457611085e8173b76e75d17505f567e3534f275
FFugi <fudifil@gmail.com>
2019-04-03 14:28:08 +0200

    Add database migrations

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
+DATABASE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/database.db'
 UPLOAD_DIR = 'files'
 SECRET_KEY = 'dev'
-TOTP_SECRET = 'dupa'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- dropspot/__init__.py
+++ dropspot/__init__.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dropspot import auth, drop, profile
+import os
+
+migrate = Migrate()
 
 
 def create_app():
@@ -14,10 +18,13 @@ def create_app():
     from dropspot.models import db
 
     db.init_app(app)
-    database = Path("./database.db")
+
+    database = Path(app.config["DATABASE_PATH"])
     if not database.is_file():
         print("no database detected, creating one")
         with app.app_context():
             db.create_all()
+
+    migrate.init_app(app, db)
 
     return app
